2771e499139b8119a36b9a30eeb779384bd97c7a
Kate2425 <kate2425.u@gmail.com>
2025-05-01 20:19:18 +0900

    環境設定を追加

--- src/main/java/com/example/twst/TwstApplication.java
+++ src/main/java/com/example/twst/TwstApplication.java
@@ -1,13 +1,25 @@
 package com.example.twst;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
-public class TwstApplication {
+@Slf4j
+public class TwstApplication implements CommandLineRunner {
+
+	@Autowired
+	private Config config;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwstApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(config.toString());
+	}
 }
