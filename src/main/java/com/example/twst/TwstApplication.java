340c0afff2266c39bca759c3189babace67314e7
Kate2425 <kate2425.u@gmail.com>
2025-05-04 00:26:37 +0900

    リファクタリング

--- src/main/java/com/example/twst/TwstApplication.java
+++ src/main/java/com/example/twst/TwstApplication.java
@@ -3,16 +3,25 @@ package com.example.twst;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import com.example.twst.config.ConfigProd;
+import com.example.twst.config.Config;
 
 import lombok.extern.slf4j.Slf4j;
 
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
 @Slf4j
 public class TwstApplication implements CommandLineRunner {
 
 	@Autowired
 	private Config config;
+
+	@Autowired
+	private ConfigProd configProd;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwstApplication.class, args);
@@ -21,5 +30,6 @@ public class TwstApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info(config.toString());
+		log.info(configProd.toString());
 	}
 }
