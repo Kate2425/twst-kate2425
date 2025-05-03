20713c6c3d2b20b44d31cb9b2b3b9d492843edc2
Kate2425 <kate2425.u@gmail.com>
2025-05-04 00:31:30 +0900

    package変更

---
+++ src/main/java/com/example/twst/config/Config.java
@@ -0,0 +1,24 @@
+package com.example.twst.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "myapp.config")
+@Component
+public class Config {
+
+    private String key;
+
+    private String Key2 = null;
+
+    private String envTest;
+
+    private String noenv;
+
+    private boolean flag;
+
+    private String env;
+}
