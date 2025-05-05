e30bf59b3fb7d9f7d6bd775962a79727d473f595
Kate2425 <kate2425.u@gmail.com>
2025-05-05 14:05:55 +0900

    add componentScan

--- src/main/java/com/example/twst/config/DataSourceConfig.java
+++ src/main/java/com/example/twst/config/DataSourceConfig.java
@@ -1,12 +1,14 @@
 package com.example.twst.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "spring.datasource")
+@ComponentScan
 @Component
 public class DataSourceConfig {
 
