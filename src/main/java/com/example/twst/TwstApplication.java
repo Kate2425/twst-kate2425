963ca3ae89c7b7682c785e4dc8629d60c0e2dfc9
Kate2425 <kate2425.u@gmail.com>
2025-05-13 12:28:13 +0900

    config delete

--- src/main/java/com/example/twst/TwstApplication.java
+++ src/main/java/com/example/twst/TwstApplication.java
@@ -1,32 +1,12 @@
 package com.example.twst;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.twst.config.DataSourceConfig;
-import com.example.twst.config.ParameterConfig;
-
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@Slf4j
-public class TwstApplication implements CommandLineRunner {
-
-	@Autowired
-	private ParameterConfig config;
-
-	@Autowired
-	private DataSourceConfig dataSource;
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info(config.toString());
-		log.info(dataSource.toString());
-	}
+public class TwstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwstApplication.class, args);
 	}
-
 }
