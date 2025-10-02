44e5b81e2581278f8668534ce80ac11f363bc553
Kate2425 <kate2425.u@gmail.com>
2025-10-02 12:35:04 +0900

    修正

--- src/test/java/com/example/twst/controller/EditControllerTest.java
+++ src/test/java/com/example/twst/controller/EditControllerTest.java
@@ -3,32 +3,28 @@ package com.example.twst.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.example.twst.TestConfig;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-@ContextConfiguration(classes = TestConfig.class)
-@AutoConfigureMockMvc
+@WebAppConfiguration
 public class EditControllerTest {
-
-    @InjectMocks
-    private EditController target;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(target)
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .alwaysDo(log()).build();
     }
 
--- src/test/java/com/example/twst/controller/InsertControllerTest.java
+++ src/test/java/com/example/twst/controller/InsertControllerTest.java
@@ -12,68 +12,72 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.example.twst.TestConfig;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.example.twst.domain.model.Card;
 import com.example.twst.domain.model.TableEnum;
 import com.example.twst.service.CardService;
+import com.example.twst.session.EditSession;
 
 @SpringBootTest
-@ContextConfiguration(classes = TestConfig.class)
-@AutoConfigureMockMvc
+@Transactional
+@WebAppConfiguration
 public class InsertControllerTest {
-
     @Autowired
+    WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
 
     @Mock
     private CardService service;
 
-    @InjectMocks
-    private InsertController target;
+    @Autowired
+    EditSession insertSession;
 
     @BeforeEach
     void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(target)
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .alwaysDo(log()).build();
     }
 
     @Test
     @DisplayName("input")
     void input_01() throws Exception {
+        //Arrange 
         Card card = new Card();
         card.setTableName(TableEnum.SENDING_STAR_DRESS);
         card.setId("sen_deuce");
         List<Card> cardList = new ArrayList<>();
         cardList.add(card);
-        when(service.selectMany(any(), anyString())).thenReturn(cardList);
+        doReturn(cardList).when(service).selectMany(any(), anyString());
 
-        mockMvc.perform(get("/insert")
+        MvcResult mvcResult = mockMvc.perform(get("/insert")
                 .param("tableNameChecks", "sending_star_dress"))
                 .andExpect(view().name("insert.html"))
                 .andExpect(status().isOk())
-                .andExpect(model().attribute("cardList", is(hasItem(hasProperty("id", is("sen_deuce"))))));
-    }
+                .andExpect(model().attribute("cardList", is(hasItem(hasProperty("id", is("sen_deuce"))))))
+                .andExpect(model().attribute("tableName", is(TableEnum.SENDING_STAR_DRESS)))
+                .andReturn();
 
-    @Test
-    @DisplayName("conform")
-    void conform() throws Exception {
+        MockHttpSession mockSession = (MockHttpSession) mvcResult.getRequest().getSession();
 
-        when(service.insert(any())).thenReturn(true);
-        mockMvc.perform(post("/insert")
+        doReturn(true).when(service).insert(any());
+
+        mvcResult = mockMvc.perform(post("/insert")
+                .session(mockSession)
                 .param("tableName", "SENDING_STAR_DRESS")
                 .param("name", "JADE")
                 .param("rare", "R")
@@ -81,9 +85,12 @@ public class InsertControllerTest {
                 .param("buddy1", "FLOYD")
                 .param("buddy2", "HYPHEN")
                 .param("buddy3", "HYPHEN")
-                .param("magic1Grouping", "1")
-                .param("magic2Grouping", "2")
-                .param("magic3Grouping", "3")
+                .param("magic1", "AQUA_WAVE")
+                .param("magic2", "FOREST_STRIKE")
+                .param("magic3", "FIRE_SHOT2")
+                .param("magic1BuffdebuffGrouping", "DAMAGE_DOWN_LARGE_ENEMY_1T")
+                .param("magic2BuffdebuffGrouping", "HYPHEN")
+                .param("magic3BuffdebuffGrouping", "ATK_UP_SMALL_SELF_1T_AND_DAMAGE_DOWN_SMALL_ENEMY_1T")
                 .param("duo", "HYPHEN")
                 .param("minHp", "0")
                 .param("minAtk", "0")
@@ -93,7 +100,20 @@ public class InsertControllerTest {
                 .param("buddy1Grouping", "3")
                 .param("buddy2Grouping", "2")
                 .param("buddy3Grouping", "1"))
+                .andExpect(status().isFound())
                 .andExpect(view().name("redirect:insert"))
-                .andExpect(status().isFound());
+                .andReturn();
+
+        insertSession = (EditSession) mockSession.getAttribute("scopedTarget.insertSession");
+
+        doReturn(cardList).when(service).selectMany(any(), anyString());
+
+        mockMvc.perform(get("/insert")
+                .session(mockSession))
+                .andExpect(view().name("insert.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("cardList", is(hasItem(hasProperty("id", is("sen_deuce"))))))
+                .andExpect(model().attribute("tableName", is(TableEnum.SENDING_STAR_DRESS)));
+
     }
 }
