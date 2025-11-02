5a3b627c87ab224d28784b5d4301ab2cfda590b7
Kate2425 <kate2425.u@gmail.com>
2025-11-03 02:18:57 +0900

    modalを修正

--- src/main/resources/static/style/edit.js
+++ src/main/resources/static/style/edit.js
@@ -3,17 +3,40 @@ window.addEventListener('load', function () {
   console.log('url:%o', url);
 
   const tableViewName = $('input[name="tableViewName"]').val();
+  let characterViewName;
+
+  $(function () {
+    if (url == '/insert') {
+      $('[name=name]').change(function () {
+        // 選択されているvalue属性値を取り出す
+        let val = $('[name=name]').val();
+        console.log(val);
+        // 選択されている表示文字列を取り出す
+        characterViewName = $('[name=name] option:selected').text();
+        console.log(characterViewName);
+      });
+    }
+  });
 
   // 送信処理
   $(function () {
     $('button').click(function () {
+      if (url != '/insert') {
+        characterViewName = $(this)
+          .parent()
+          .parent()
+          .find('input[name="characterViewName"]')
+          .val();
+        console.log(characterViewName);
+      }
+
       let message = '';
       if (url == '/insert') {
-        message = 'に登録しました。';
+        message = tableViewName + ' ' + characterViewName + 'を登録しました。';
       } else if (url == '/update') {
-        message = 'を更新しました。';
+        message = tableViewName + ' ' + characterViewName + 'を更新しました。';
       } else if (url == '/delete') {
-        message = 'から削除しました。';
+        message = tableViewName + ' ' + characterViewName + 'を削除しました。';
       }
 
       let form = $(this).parent().parent().find('form');
@@ -22,7 +45,7 @@ window.addEventListener('load', function () {
       // フォーム送信
       form.submit();
       $('.modal-body').find('p').remove(); //モーダルウィンドウの表示前に<p>タグを削除する
-      $('.modal-body').append('<p>' + tableViewName + message + '</p>'); //<p>タグの追加。
+      $('.modal-body').append('<p>' + message + '</p>'); //<p>タグの追加。
       $('#successMsg').modal('show'); //モーダルウィンドウを表示する
     });
   });
